9ac57fc59a547f4e94d3e13255679974201709b2
Konrad Szeromski <cnr.roxx@gmail.com>
2018-07-23 12:35:54 +0200

    Multiple entry points

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -2,6 +2,11 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +14,4 @@ public class Application {
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(Application.class, args);
     }
-
 }
